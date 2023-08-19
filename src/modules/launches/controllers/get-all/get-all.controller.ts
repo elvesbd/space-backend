73addb7b813906741d357d9f0624ad0ae0a42c85
elvesbd <elvesbd@yahoo.com>
2023-08-18 21:56:41 -0300

    feat: implemented search for launches

--- src/modules/launches/controllers/get-all/get-all.controller.ts
+++ src/modules/launches/controllers/get-all/get-all.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { LaunchesResponseDto } from '../../dto';
+import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
+import { FiltersDto, LaunchesResponseDto } from '../../dto';
+import { GetAllService } from '../../application/services/get-all';
 
 @Controller()
 export class GetAllController {
+  constructor(private readonly getAllService: GetAllService) {}
+
   @Get('launches')
-  getAll(): Promise<LaunchesResponseDto> {
-    return null;
+  getAll(
+    @Query('search') search: string,
+    @Query('limit', ParseIntPipe) limit: number,
+  ): Promise<LaunchesResponseDto> {
+    const filters: FiltersDto = { search, limit };
+    return this.getAllService.execute(filters);
   }
 }
