c0fabe7b415212223d6a7c6101d86884cc309642
elvesbd <elvesbd@yahoo.com>
2023-08-19 09:17:29 -0300

    test: created setup for unit tests

--- src/modules/launches/application/services/get-all/get-all.service.spec.ts
+++ src/modules/launches/application/services/get-all/get-all.service.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GetAllService } from './get-all.service';
+import { LaunchesRepository } from 'src/modules/launches/repositories';
 
 describe('GetAllService', () => {
   let sut: GetAllService;
+  let launchesRepository: LaunchesRepository;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const LaunchesRepositoryProvider = {
+      provide: 'LAUNCHES_REPOSITORY',
+      useValue: {
+        getAll: jest.fn().mockResolvedValue(null),
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GetAllService],
+      providers: [GetAllService, LaunchesRepositoryProvider],
     }).compile();
 
     sut = module.get<GetAllService>(GetAllService);
+    launchesRepository = module.get<LaunchesRepository>('LAUNCHES_REPOSITORY');
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
+    expect(launchesRepository).toBeDefined();
   });
 });
