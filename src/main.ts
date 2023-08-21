9306dc75f9201af134c19f716488aa7fb702dd93
elvesbd <elvesbd@yahoo.com>
2023-08-21 10:38:47 -0300

    feat:  enable swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { createDocument, customOptions } from './modules/shared/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
+  SwaggerModule.setup('api/v1', app, createDocument(app), customOptions);
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/modules/shared/swagger/swagger.config.ts
@@ -0,0 +1,8 @@
+import { SwaggerConfig } from './swagger.interface';
+
+export const SWAGGER_CONFIG: SwaggerConfig = {
+  title: 'Api-Launches',
+  description: `Descrição`,
+  version: '1.0',
+  tags: [''],
+};
