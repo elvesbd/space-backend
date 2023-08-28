cb5d6739029c612be33ab2d662079cad15181fe1
elvesbd <elvesbd@yahoo.com>
2023-08-28 07:48:01 -0300

    test: ensures be called launchesRepository.getAllLaunches with correct value

---
+++ src/modules/rocket-launches/application/services/get-launch-chart-data/get-launch-chart-data.service.spec.ts
@@ -0,0 +1,45 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { LaunchesRepository } from 'src/modules/rocket-launches/repositories';
+import { LaunchDataBuilder } from '../../../__mocks__/data-builder';
+import { GetLaunchChartDataService } from './get-launch-chart-data.service';
+import { YearlyRocketCountsDataBuilder } from '../../../__mocks__/data-builder/yearly-rocket-count-data.builder';
+
+describe('GetLaunchChartDataService', () => {
+  let sut: GetLaunchChartDataService;
+  let launchesRepository: LaunchesRepository;
+
+  const launches = LaunchDataBuilder.aLaunch().build();
+  const yearlyRocketCounts =
+    YearlyRocketCountsDataBuilder.aYearlyRocketCounts().build();
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const LaunchesRepositoryProvider = {
+      provide: 'LAUNCHES_REPOSITORY',
+      useValue: {
+        getAllLaunches: jest.fn().mockResolvedValue([launches]),
+      },
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [GetLaunchChartDataService, LaunchesRepositoryProvider],
+    }).compile();
+
+    sut = module.get<GetLaunchChartDataService>(GetLaunchChartDataService);
+    launchesRepository = module.get<LaunchesRepository>('LAUNCHES_REPOSITORY');
+  });
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
+    expect(launchesRepository).toBeDefined();
+  });
+
+  describe('handle()', () => {
+    it('should be called launchesRepository.getAllLaunches with correct value', async () => {
+      await sut.handle();
+      expect(launchesRepository.getAllLaunches).toHaveBeenCalledTimes(1);
+      expect(launchesRepository.getAllLaunches).toHaveBeenCalledWith();
+    });
+  });
+});
